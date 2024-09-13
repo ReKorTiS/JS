6bf63371ab8fec165a8b982a26bdd50bd9dbad51
ReKorTiS <rekortis@mail.ru>
2024-09-13 09:15:45 +0300

    Add files via upload

---
+++ fg.js
@@ -0,0 +1,24 @@
+let year = 365
+let days = 30
+let hour = 24
+let minute = 60
+let sec = 60
+console.log(`В сутках ${hour*minute*sec} секунд`)
+console.log(`В 30 сутках ${days*hour*minute*sec} секунд`)
+console.log(`В году ${year*hour*minute*sec} секунд`)
+console.log(`В сутках ${hour*minute} минут`)
+console.log(`В году ${year*hour*minute} минут`)
+let bait = 1
+let onekb = 1
+let kb = 1024
+let mb = 1024
+let gb = 1024 
+let tengb = 1024*10
+let tb = 1024
+console.log(`В мегабайте ${bait*kb*mb} байт `)
+console.log(`В гигабайте ${bait*kb*mb*gb} байт`)
+console.log(`В 10 гигабайтах ${bait*kb*mb*tengb} байт`)
+console.log(`В терабайте ${bait*kb*mb*gb*tb} байт`)
+console.log(`В терабайте ${onekb*mb*gb*tb} килобайт`)
+
+
